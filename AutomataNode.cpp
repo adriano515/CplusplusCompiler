b9a7127c8ff2bbb1480c20472fd4e30a873b985f
adriano515 <valdezadriano@gmail.com>
2016-09-04 15:22:52 -0600

    finished classes , added epsilon, letter and automataOr methods for generating automatas related to name Todo: andAutomata, kleenAutomata

--- AutomataNode.cpp
+++ AutomataNode.cpp
@@ -3,20 +3,131 @@
 #include <string>
 
 using namespace std;
+class Automata{
+    AutomataNode* initial;
+    vector<AutomataNode*> final;
+public:
+    Automata(AutomataNode* init, vector<AutomataNode*> fin){
+        initial = init;
+        final = fin;
+    }
+
+    Automata(){}
+
+    void addAutomataNodes(AutomataNode* node,bool b) {
+        //where true = add initial and fals= add final
+        if (b) {
+            initial= node;
+        }
+        else{
+            final.push_back(node);
+        }
+    }
+    AutomataNode* getInitNode(){
+        return initial;
+    }
+
+    vector<AutomataNode*> getFinalNodes(){
+        return final;
+    }
+
+};
+
 
 class AutomataNode{
-
-    vector Transition;
+private:
+    vector<Transition*> Itransitions;
+    vector<Transition*> Ftransitions;
     int number;
+public:
+    void setAutomataNumber(int num){
+        number = num;
+    }
+
+    void addAutomataTran(Transition* t,bool b) {
+        //where true = add initial and fals= add final
+        if (b) {
+            Itransitions.push_back(t);
+        }
+        else{
+            Ftransitions.push_back(t);
+        }
+    }
 
 };
 
 class Transition{
-    AutomataNode initial;
-    AutomataNode final;
+private:
+    AutomataNode* initial;
+    AutomataNode* final;
     string transition;
+public:
+    Transition(AutomataNode* init, AutomataNode* fin, string tran){
+        initial = init;
+        final = fin;
+        transition = tran;
+    }
 };
 
-AutomataNode
 
+
+Automata *epsilon(AutomataNode *i, vector<AutomataNode *> f) {
+    Automata *automata = new Automata();
+    for(int j=0;j<f.size();j++) {
+
+
+        Transition *t = new Transition(i, f[j], "e");
+
+        i->addAutomataTran(t, true);
+        f[j]->addAutomataTran(t, false);
+
+        automata->addAutomataNodes(f[j], false);
+    }
+
+    automata->addAutomataNodes(i, true);
+
+    return automata;
+}
+
+Automata *letter(AutomataNode *i, vector<AutomataNode *> f, string letter) {
+    Automata *automata = new Automata();
+    for(int j=0;j<f.size();j++) {
+
+
+        Transition *t = new Transition(i, f[j], letter);
+
+        i->addAutomataTran(t, true);
+        f[j]->addAutomataTran(t, false);
+
+        automata->addAutomataNodes(f[j], false);
+    }
+
+    automata->addAutomataNodes(i, true);
+
+    return automata;
+}
+
+
+AutomataNode *automataOR(Automata *a, Automata *b) {
+    AutomataNode* initial = new AutomataNode(); //initial node
+    AutomataNode* uFNode = a->getInitNode(); //upper first node
+    AutomataNode* lFNode = b->getInitNode(); //lower first node
+    AutomataNode* final = new AutomataNode(); //final automata
+
+    vector<AutomataNode*> temp;
+    temp.push_back(uFNode);
+    temp.push_back(lFNode);
+
+    vector<AutomataNode*> temp2;
+    temp2 = a->getFinalNodes();
+    for (int i =0;i<b->getFinalNodes().size();i++){
+        temp2.push_back(b->getFinalNodes()[i]);
+    }
+
+    epsilon(initial,temp);
+    epsilon(final,temp2);
+
+    Automata* automata = new Automata(initial,temp2);
+
+}
 
--- AutomataNode.h
+++ AutomataNode.h
@@ -5,4 +5,18 @@
 #ifndef C_COMPILER_AUTOMATANODE_H
 #define C_COMPILER_AUTOMATANODE_H
 
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Automata;
+class AutomataNode;
+class Transition;
+
+Automata* epsilon(AutomataNode* i, vector<AutomataNode*> f);
+Automata* letter(AutomataNode* i, vector<AutomataNode*> f,string letter);
+AutomataNode *automataOR(Automata *a, Automata *b);
+
+
 #endif //C_COMPILER_AUTOMATANODE_H
