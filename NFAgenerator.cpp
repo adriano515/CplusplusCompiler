c080e551c8d8fd398b44852b3c9859d124ef7140
adriano515 <valdezadriano@gmail.com>
2016-09-04 19:06:50 -0600

    Added files for NFA generation

---
+++ NFAgenerator.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Freddie on 9/4/2016.
+//
+
+#include "NFAgenerator.h"
+#include "AutomataNode.h"
+
+Automata* foundLetter(char letter){
+    AutomataNode* init = new AutomataNode();
+    AutomataNode* final = new AutomataNode();
+
+    automataLetter(init,final,letter)
+}
---
+++ NFAgenerator.h
@@ -0,0 +1,8 @@
+//
+// Created by Freddie on 9/4/2016.
+//
+
+#ifndef C_COMPILER_NFAGENERATOR_H
+#define C_COMPILER_NFAGENERATOR_H
+
+#endif //C_COMPILER_NFAGENERATOR_H
