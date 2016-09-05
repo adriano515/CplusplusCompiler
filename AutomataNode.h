2c9141cad571785f271f5f79b023d8fc1ee637a4
adriano515 <valdezadriano@gmail.com>
2016-09-04 18:53:16 -0600

    Added missing methods for header

--- AutomataNode.h
+++ AutomataNode.h
@@ -14,9 +14,12 @@ class Automata;
 class AutomataNode;
 class Transition;
 
-Automata* epsilon(AutomataNode* i, vector<AutomataNode*> f);
-Automata* letter(AutomataNode* i, vector<AutomataNode*> f,string letter);
-AutomataNode *automataOR(Automata *a, Automata *b);
+Automata* automataEpsilon(AutomataNode* i, vector<AutomataNode*> f);
+Automata *automataEpsilon(AutomataNode *i,AutomataNode *f);
+Automata* automataLetter(AutomataNode* i, vector<AutomataNode*> f,string letter);
+Automata* automataOR(Automata *a, Automata *b);
+Automata *automataAnd(Automata *a, Automata *b);
+Automata *automataKleen(Automata *a);
 
 
 #endif //C_COMPILER_AUTOMATANODE_H
