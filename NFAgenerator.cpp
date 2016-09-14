869283ff896786ff00d60189195585845de6fb11
adriano515 <valdezadriano@gmail.com>
2016-09-14 17:16:51 -0600

    Added each node number to the nfa generator (each node now has a unique number starting at 0)

--- NFAgenerator.cpp
+++ NFAgenerator.cpp
@@ -6,6 +6,7 @@
 #include "AutomataNode.h"
 #include "TreeNode.h"
 #include "postfix.h"
+int counter = 0;
 
 
 
@@ -14,6 +15,12 @@ Automata *nfaGen(TreeNode *current) {
     if(!isOperator(current->getValue())){
         AutomataNode* a = new AutomataNode();
         AutomataNode* b = new AutomataNode();
+
+        a->setNumber(counter);
+        counter = counter + 1;
+        b->setNumber(counter);
+        counter = counter + 1;
+
         current->setAutomata(letterAut(a,b,current->getValue()));
         return current->getAutomata();
     }
@@ -28,12 +35,24 @@ Automata *nfaGen(TreeNode *current) {
         if (current->getValue() == '^') {
             Automata *leftAutom = nfaGen(current->getLeft());
             current->setAutomata(kleenAutomata(leftAutom));
+
+            current->getAutomata()->getInitNode()->setNumber(counter);
+            counter = counter + 1;
+            current->getAutomata()->getFinNode()->setNumber(counter);
+            counter = counter + 1;
+
             return current->getAutomata();
         }
         if (current->getValue() == '|') {
             Automata *leftAutom = nfaGen(current->getLeft());
             Automata *rightAutom = nfaGen(current->getRight());
             current->setAutomata(orAut(leftAutom, rightAutom));
+
+            current->getAutomata()->getInitNode()->setNumber(counter);
+            counter = counter + 1;
+            current->getAutomata()->getFinNode()->setNumber(counter);
+            counter = counter + 1;
+
             return current->getAutomata();
         }
     }
