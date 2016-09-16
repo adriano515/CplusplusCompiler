fae5f8c59bd0aa6442a3e1ba2d9b7bc5437f06c0
adriano515 <valdezadriano@gmail.com>
2016-09-16 17:34:13 -0600

    deleted unused variables, added a for to determine the alphabet of an input

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "TreeNode.h"
 #include "NFAgenerator.h"
 #include "NFAsimulation.h"
+#include "Nfa2Dfa.h"
 
 using namespace std;
 
@@ -12,6 +13,7 @@ int main(){
     string sentence ;
     string postfix;
     string text;
+    string language;
 
     cout <<"Escribir cadena ";
 
@@ -19,24 +21,35 @@ int main(){
 
     cout << "Output =" << infix2Postfix(sentence) << "\n";
 
+    for(int i =0; i<sentence.size(); i++) {
+        if (isOperand(sentence[i])) {
+            if (language.find(sentence[i]) == string::npos) {
+                language += sentence[i];
+            }
+        }
+    }
+
+    cout <<"lenguaje contiene cadenas de " << language<< endl;
+
     postfix = infix2Postfix(sentence);
 
     stack < TreeNode * > t=postfix2Tree(postfix);
 
-    TreeNode* tree = t.top();
     int tnum =0;
     int *num = &tnum;
     Automata *a = nfaGen(t.top(), num);
 
     cout <<"Escribir cadena a verificar ";
-
-    getline(cin, text);
+    /*getline(cin, text);
     if (nfaSimulation(text,a)==true){
         cout << "El resultado es: si se acepta la cadena" << endl;
     }
     else if(nfaSimulation(text,a)==false) {
         cout << "El resultado es: no se acepta la cadena" << endl;
     }
+    */
+    Nfa2Dfa(a,language);
+
     return 0;
 
 }
